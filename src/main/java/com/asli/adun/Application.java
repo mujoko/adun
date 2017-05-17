94338e25af692fe968edd551faa8a437eb0500e0
mujoko <mujoko.mujoko@gmail.com>
2017-05-17 15:45:28 +0800

    having one project

--- src/main/java/com/asli/adun/Application.java
+++ src/main/java/com/asli/adun/Application.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
